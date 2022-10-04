3d6a9ad3f52367c3b1a4619f6ea3af9b23c5824d
Sujay509 <sujaychauhan98@gmail.com>
2022-10-04 15:49:40 +0530

    Welcome to Employee Wage Computation Program

---
+++ src/com/bridgelabz/empWage/Hello.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.empWage;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome To Employee Wage Computation Program");
+	}
+
+}
